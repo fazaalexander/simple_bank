b941e7977864f8a9488c906b3d35db883073b305
fazaalexander <fazaalexander@gmail.com>
2024-09-22 19:36:12 +0700

    simplebank initial commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5433/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+// var testDB *pgxpool.Pool
+
+func TestMain(m *testing.M) {
+	// var err error
+	// connPool, err = pgxpool.New(context.Background(), dbSource)
+	// if err != nil {
+	// 	log.Fatal("Cannot connect to the db:", err)
+	// }
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to the db:", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
